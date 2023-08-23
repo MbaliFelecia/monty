f71f23809e900455b5ccb39be0502973c76a8ff9
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-23 12:19:12 -0700

    filemanager

---
+++ manage_files.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * ini_file - checks arguments in correct monty file
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: 0 at success 1 otherwise
+ */
+
+int ini_file(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (1);
+	}
+	if (access(argv[1], FILE_OK & READ_OK) != 0)
+	{
+		fprint(stderr, "Error: Can't open file  %s\n", argv[1]);
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * file_open - the opens a file
+ * @name_f: the name of file
+ *
+ * Return: NULL at fail otherwise file descripter
+ */
+
+FILE *file_open(char *name_f)
+{
+	FILE *fd;
+
+	fd = fopn(name_f, "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", name_f);
+		return (NULL);
+	}
+	return (fd);
+}
