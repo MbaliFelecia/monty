619ed44f825fabfdd7b9230876bdc42248f2acd2
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-23 09:25:22 -0700

    addedopcode

--- monty.h
+++ monty.h
@@ -42,6 +42,23 @@ typedef struct instruction_s
 /* Global variable for the stack head */
 stack_t *stack_head = NULL;
 
-
+/* opcode function */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stcak, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stcak, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void mul(stcak_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
 #endif 
