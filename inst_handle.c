9510c7de19994d739d3ef920f7e4a0ee80546f21
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-23 23:30:29 -0700

    instruction

---
+++ inst_handle.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * rd_line - reads lines from a file
+ * @bytecode_file: the pointer
+ *
+ * Return: the lines content
+ */
+
+char *rd_line(FILE *bytecode_file)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssizte_t real_read;
+
+	real_read = getline(&line, &len, bytecode_file);
+	if (real_read == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
