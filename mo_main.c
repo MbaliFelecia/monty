a58649a4ca4fd97252594df9bdb61e47b030e58d
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-24 04:49:57 -0700

    mainfile

--- mo_main.c
+++ mo_main.c
@@ -14,21 +14,21 @@ global_data_t globe_data;
 int main(int argc, char **argv)
 {
 	int file_status;
-	FILE *bc_file;
+	FILE *bytecode_file;
 	char *opcode_tokens[2] = {NULL, NULL};
-	void (*op_f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 
 	file_status = ini_file(argc, argv);
 	if (file_status == 1)
 		exit(EXIT_FAILURE);
 	
-	bc_file = file_op(argv[1]);
+	bytecode_file = file_open(argv[1]);
 	if (bc_file == NULL)
 		exit(EXIT_FAILURE);
 
 	global_data();
-	globe.bc_file = bc_file;
-	globe.buff = rd_line(bc_file);
+	globe.bytecode_file = bytecode_file;
+	globe.buff = rd_line(bytecode_file);
 	while (globe.buff)
 	{
 		opcode_tokens[0] = strtok(globe.buff, " \t\n");
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 			f = get_opcode(opcode_tokens[0]);
 			if (f == NULL)
 			{
-				fprintf(stderr, "L%u: " globe.count_line);
+				fprintf(stderr, "L%u: ", globe.count_line);
 				fprintf(stderr, "unkown instruction %s\n", opcode_tokens[0]);
 				trm_global();
 				exit(EXIT_FAILURE);
