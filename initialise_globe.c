6812d0672b2e30522064a68be66b37f182182890
Tillymolete <Mahlatsemolete@gmail.com>
2023-08-23 23:52:08 -0700

    initialglobe

---
+++ initialise_globe.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * global_data - initialise globral variable
+ */
+
+void global_data(void)
+{
+	globe.q_s_alt = 1;
+	globe.count_line = 1;
+	globe.arg = NULL;
+	globe.bytecode_file = NULL;
+	globe.head = NULL;
+}
+
+/**
+ * trm_global - terminates global variable
+ */
+
+void trm_global(void)
+{
+	free_dlistint(globe.head);
+	free(globe.buff);
+	fclose(globe.bytecode_file);
+}
